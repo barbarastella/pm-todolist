a87d7aabd6a4025d2547eda2841ed86f0cbe0f63
Bárbara Stella <barbarastellaw@gmail.com>
2026-03-12 09:28:18 -0300

    feat: implementação de busca de endereço por CEP consumindo serviço ViaCEP

---
+++ lib/screens/gifs/gifs.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Gifs extends StatefulWidget {
+
+  @override
+  State createState() {
+    return;
+  }
+} 
---
+++ lib/screens/gifs/gifs_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class GifsPage extends StatefulWidget {
+
+  @override
+  State createState() {
+    return;
+  }
+}
